8980482b270d98fc9ec2146e40c490b53af3044e
Robert Ashton <r.ashton@imperial.ac.uk>
2020-01-09 10:31:55 +0000

    Try to use probot to make a bot

---
+++ naomi-probot/index.js
@@ -0,0 +1,26 @@
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Application} app
+ */
+module.exports = app => {
+  // Your code here
+  app.log('Yay, the app was loaded!')
+
+  app.on('issues.opened', async context => {
+    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    return context.github.issues.createComment(issueComment)
+  })
+
+  // When a new pull request is received (and is ready for review)
+  app.on('pull_request.opened', async context => {
+    
+  })
+  // and the pull request updates the version
+  // Update code in hintr and create a PR for that
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+}
